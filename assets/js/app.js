68d828c4db1848084e3952969c169b7693424edc
Neofyt <gtouzeau17@gmail.com>
2014-03-11 14:56:42 +0100

    Some modifications...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
-
-
-var option = "<option value='{0}'>{1}</option>";
+var templates = {
+	option : "<option value='{0}'>{0}</option>",
+	message : "Vous avez <span>{0}%</span> de chance d'arriver à l'heure !!"
+};
 
 String.prototype.format = function(){
 	var string = this;
@@ -10,17 +11,57 @@ String.prototype.format = function(){
 	return string;
 };
 
+Array.prototype.has = function(v) {
+	for (var i = 0, length = this.length; i < length; i++) {
+		if (this[i] == v) return true;
+	}
+	return false;
+};
 
-
-
-function getArrivals(depart){
-	var arrivals = [];
+function getDeparts() {
+	var departs = new Array;
 	for (var prop in data) {
-		if (data[prop][0] === depart) {
-			arrivals.push(data[prop][1]);
+		var gare = data[prop][0];
+		if(!departs.has(gare) && gare !== undefined){
+			departs.push(gare);
 		}
 	}
-	return arrivals;
+	return departs;
 }
 
-var arrivals = getArrivals("PARIS MONTPARNASSE");
+function getArrivees(depart) {
+	depart = depart.toUpperCase();
+	var arrivees = [];
+	for (var i = 0; i < data.length; i++){
+		if (data[i][0] === depart) {
+			arrivees.push(data[i][1]);
+		}
+	}
+	return arrivees;
+}
+
+function createList(array) {
+	tpl = "";
+	for (var i = 0, length = array.length; i < length; i++){
+		tpl += templates.option.format(array[i]);
+	}
+	return tpl;
+}
+
+function populateArrivee(){
+	arrivee.innerHTML = createList(getArrivees(depart.value));
+}
+
+function setProgress(){
+	for (var prop in data) {
+		if(data[prop][0] == depart.value.toUpperCase() && data[prop][1] == arrivee.value.toUpperCase()){
+			var percent = 100 - ((data[prop][3]*100) / data[prop][2]);
+			indicator.style.width = percent + "%";
+			result.innerHTML = templates.message.format(Math.round(percent));
+		}
+	}
+}
+
+depart.innerHTML = createList(getDeparts());
+arrivee.innerHTML = createList(getArrivees(depart.value));
+setProgress();
